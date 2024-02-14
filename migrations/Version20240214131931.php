acc480772a02d54598dd159b4fdc6dc21656ade5
frev95 <gofrev@gmail.com>
2024-02-14 14:36:11 +0100

    Migration

---
+++ migrations/Version20240214131931.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240214131931 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE category (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name VARCHAR(40) NOT NULL)');
+        $this->addSql('CREATE TABLE feature (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, type_id INTEGER DEFAULT NULL, name VARCHAR(80) NOT NULL, description CLOB DEFAULT NULL)');
+        $this->addSql('CREATE INDEX IDX_1FD77566C54C8C93 ON feature (type_id)');
+        $this->addSql('CREATE TABLE image (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, room_id INTEGER NOT NULL, url VARCHAR(500) NOT NULL)');
+        $this->addSql('CREATE INDEX IDX_C53D045F54177093 ON image (room_id)');
+        $this->addSql('CREATE TABLE reservation (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, room_id INTEGER NOT NULL, user_id INTEGER NOT NULL, start_date DATETIME NOT NULL, end_date DATETIME NOT NULL, status INTEGER NOT NULL)');
+        $this->addSql('CREATE INDEX IDX_42C8495554177093 ON reservation (room_id)');
+        $this->addSql('CREATE INDEX IDX_42C84955A76ED395 ON reservation (user_id)');
+        $this->addSql('CREATE TABLE room (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, category_id INTEGER DEFAULT NULL, name VARCHAR(80) NOT NULL, description CLOB NOT NULL, capacity INTEGER NOT NULL)');
+        $this->addSql('CREATE INDEX IDX_729F519B12469DE2 ON room (category_id)');
+        $this->addSql('CREATE TABLE room_feature (room_id INTEGER NOT NULL, feature_id INTEGER NOT NULL, PRIMARY KEY(room_id, feature_id))');
+        $this->addSql('CREATE INDEX IDX_F3F5C98654177093 ON room_feature (room_id)');
+        $this->addSql('CREATE INDEX IDX_F3F5C98660E4B879 ON room_feature (feature_id)');
+        $this->addSql('CREATE TABLE type (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name VARCHAR(40) NOT NULL)');
+        $this->addSql('CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles CLOB NOT NULL --(DC2Type:json)
+        , password VARCHAR(255) NOT NULL, name VARCHAR(80) NOT NULL, phone VARCHAR(30) NOT NULL, role VARCHAR(30) NOT NULL)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649E7927C74 ON user (email)');
+        $this->addSql('CREATE TABLE messenger_messages (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, body CLOB NOT NULL, headers CLOB NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL)');
+        $this->addSql('CREATE INDEX IDX_75EA56E0FB7336F0 ON messenger_messages (queue_name)');
+        $this->addSql('CREATE INDEX IDX_75EA56E0E3BD61CE ON messenger_messages (available_at)');
+        $this->addSql('CREATE INDEX IDX_75EA56E016BA31DB ON messenger_messages (delivered_at)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE category');
+        $this->addSql('DROP TABLE feature');
+        $this->addSql('DROP TABLE image');
+        $this->addSql('DROP TABLE reservation');
+        $this->addSql('DROP TABLE room');
+        $this->addSql('DROP TABLE room_feature');
+        $this->addSql('DROP TABLE type');
+        $this->addSql('DROP TABLE user');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
